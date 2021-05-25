c86f159ce71a35261bd4a65ce09e0c9271380d67
Iain Scott <iscott@cantab.net>
2021-05-25 15:03:55 +0100

    Remove extract options from main because we will probably do this differently. (#17)

--- src/Application/Program.cs
+++ src/Application/Program.cs
@@ -2,7 +2,6 @@
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices;
-using System.Threading.Tasks;
 using Symbolica.Application;
 using Symbolica.Application.Collection;
 using Symbolica.Application.Computation;
@@ -12,50 +11,32 @@ using Symbolica.Deserialization;
 using Symbolica.Execution;
 using Symbolica.Implementation;
 using Symbolica.Implementation.System;
-using Symbolica.Representation;
 
-return args.Contains("--extract-options")
-    ? PrintExtractOptions()
-    : await Execute(args[0],
-        args.Contains("--use-symbolic-garbage"),
-        args.Contains("--use-symbolic-addresses"),
-        args.Contains("--use-symbolic-continuations"));
+IFileSystem fileSystem = RuntimeInformation.IsOSPlatform(OSPlatform.Linux)
+    ? new FileSystem()
+    : RuntimeInformation.IsOSPlatform(OSPlatform.Windows)
+        ? new WslFileSystem(new FileSystem())
+        : throw new Exception("Platform is unsupported.");
 
-static int PrintExtractOptions()
-{
-    var pattern = string.Join('|', DeclarationMapper.Patterns);
-    Console.Write($"-delete -ralias \"{pattern}\" -rfunc \"{pattern}\"");
+var collectionFactory = new CollectionFactory();
+var spaceFactory = new SpaceFactory(new SymbolFactory(), new ModelFactory(), collectionFactory);
+var programFactory = new ProgramFactory(fileSystem, spaceFactory, collectionFactory);
 
+var executor = new Executor(programFactory);
+
+await using var stream = File.OpenRead(args[0]);
+var module = await Deserializer.DeserializeModule(stream);
+
+using var programPool = new ProgramPool();
+var result = await executor.Run(programPool, module,
+    args.Contains("--use-symbolic-garbage"),
+    args.Contains("--use-symbolic-addresses"),
+    args.Contains("--use-symbolic-continuations"));
+
+if (result.IsSuccess)
     return 0;
-}
 
-static async Task<int> Execute(string path,
-    bool useSymbolicGarbage, bool useSymbolicAddresses, bool useSymbolicContinuations)
-{
-    IFileSystem fileSystem = RuntimeInformation.IsOSPlatform(OSPlatform.Linux)
-        ? new FileSystem()
-        : RuntimeInformation.IsOSPlatform(OSPlatform.Windows)
-            ? new WslFileSystem(new FileSystem())
-            : throw new Exception("Platform is unsupported.");
+Console.WriteLine(result.Message);
+Console.WriteLine(string.Join(", ", result.Example.Select(p => $"{p.Key}={p.Value}")));
 
-    var collectionFactory = new CollectionFactory();
-    var spaceFactory = new SpaceFactory(new SymbolFactory(), new ModelFactory(), collectionFactory);
-    var programFactory = new ProgramFactory(fileSystem, spaceFactory, collectionFactory);
-
-    var executor = new Executor(programFactory);
-
-    await using var stream = File.OpenRead(path);
-    var module = await Deserializer.DeserializeModule(stream);
-
-    using var programPool = new ProgramPool();
-    var result = await executor.Run(programPool, module,
-        useSymbolicGarbage, useSymbolicAddresses, useSymbolicContinuations);
-
-    if (result.IsSuccess)
-        return 0;
-
-    Console.WriteLine(result.Message);
-    Console.WriteLine(string.Join(", ", result.Example.Select(p => $"{p.Key}={p.Value}")));
-
-    return 1;
-}
+return 1;
