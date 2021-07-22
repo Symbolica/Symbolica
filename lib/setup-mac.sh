ce6580303d9f41c7d59227736e3fa369b48b2172
Iain Scott <iscott@cantab.net>
2021-07-22 02:04:22 +0100

    Remove sudo from install.

--- lib/setup-mac.sh
+++ lib/setup-mac.sh
@@ -3,6 +3,6 @@ curl -O -sSL https://desktop.docker.com/mac/stable/amd64/Docker.dmg
 hdiutil attach Docker.dmg
 cp -R /Volumes/Docker/Docker.app /Applications
 open /Applications/Docker.app
-sudo /Applications/Docker.app/Contents/MacOS/Docker --quit-after-install --unattended
+/Applications/Docker.app/Contents/MacOS/Docker --quit-after-install --unattended
 nohup /Applications/Docker.app/Contents/MacOS/Docker --unattended &
 hdiutil detach /Volumes/Docker
