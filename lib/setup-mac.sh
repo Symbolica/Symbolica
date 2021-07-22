bd29cf684e801301041c2f77efea1aae253aa441
Iain Scott <iscott@cantab.net>
2021-07-22 03:28:16 +0100

    Remove extra environment set.

--- lib/setup-mac.sh
+++ lib/setup-mac.sh
@@ -5,4 +5,3 @@ mkdir -p ~/.docker/machine/cache
 curl -Lo ~/.docker/machine/cache/boot2docker.iso https://github.com/boot2docker/boot2docker/releases/download/v19.03.12/boot2docker.iso
 brew install docker docker-machine
 docker-machine create --driver virtualbox default
-docker-machine env default
