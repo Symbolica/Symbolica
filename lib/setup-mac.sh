9009104762604223d4718413cafc4dc326476afa
Iain Scott <iscott@cantab.net>
2021-07-22 01:41:12 +0100

    Combine all the bits.

--- lib/setup-mac.sh
+++ lib/setup-mac.sh
@@ -1,6 +1,8 @@
 brew install docker
 curl -O -sSL https://desktop.docker.com/mac/stable/amd64/Docker.dmg
-open -W Docker.dmg
-cp -r /Volumes/Docker/Docker.app /Applications
+hdiutil attach Docker.dmg
+cp -R /Volumes/Docker/Docker.app /Applications
+open /Applications/Docker.app
 sudo /Applications/Docker.app/Contents/MacOS/Docker --quit-after-install --unattended
 nohup /Applications/Docker.app/Contents/MacOS/Docker --unattended &
+hdiutil detach /Volumes/Docker
