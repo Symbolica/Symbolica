af656ea3a14759c7b3141684e97a5ce969c22d27
Iain Scott <iscott@cantab.net>
2021-07-22 02:34:18 +0100

    Try the other way again but using explicit boot2docker.

--- lib/setup-mac.sh
+++ lib/setup-mac.sh
@@ -1,8 +1,8 @@
-brew install docker
-curl -O -sSL https://desktop.docker.com/mac/stable/amd64/Docker.dmg
-hdiutil attach Docker.dmg
-cp -R /Volumes/Docker/Docker.app /Applications
-open /Applications/Docker.app
-sudo /Applications/Docker.app/Contents/MacOS/Docker --quit-after-install --unattended
-nohup /Applications/Docker.app/Contents/MacOS/Docker --unattended &
-hdiutil detach /Volumes/Docker
+#!/bin/bash
+set -e
+
+mkdir -p ~/.docker/machine/cache
+curl -Lo ~/.docker/machine/cache/boot2docker.iso https://github.com/boot2docker/boot2docker/releases/download/v19.03.12/boot2docker.iso
+brew install docker docker-machine
+docker-machine create --driver virtualbox default
+docker-machine env default
--- src/Application/Executor.cs
+++ src/Application/Executor.cs
@@ -27,11 +27,11 @@ namespace Symbolica.Application
 
             await CallExternalProcess(directory, buildImage == null
                 ? "./symbolica.sh"
-                : $"sudo docker run -v $(pwd):/code {buildImage}");
+                : $"docker run -v $(pwd):/code {buildImage}");
 
             await CallExternalProcess(directory, translateImage == null
                 ? $"~/.symbolica/translate \"{DeclarationMapper.Pattern}\""
-                : $"sudo docker run -v $(pwd):/code {translateImage} \"{DeclarationMapper.Pattern}\"");
+                : $"docker run -v $(pwd):/code {translateImage} \"{DeclarationMapper.Pattern}\"");
 
             await using var stream = File.OpenRead(Path.Combine(directory, "symbolica.json"));
 
