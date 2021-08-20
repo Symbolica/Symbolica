54c541b3726bc3ee110726418a2fa40d01f338d9
Iain Scott <iscott@cantab.net>
2021-08-20 20:04:50 +0100

    Split integer and float representations so decimal and exponential notations can be accepted in addition to current hexadecimal notation.

---
+++ src/Representation/Operands/ConstantInteger.cs
@@ -0,0 +1,23 @@
+﻿using System.Numerics;
+using Symbolica.Abstraction;
+using Symbolica.Expression;
+
+namespace Symbolica.Representation.Operands
+{
+    public sealed class ConstantInteger : IOperand
+    {
+        private readonly Bits _size;
+        private readonly BigInteger _value;
+
+        public ConstantInteger(Bits size, BigInteger value)
+        {
+            _size = size;
+            _value = value;
+        }
+
+        public IExpression Evaluate(IState state)
+        {
+            return state.Space.CreateConstant(_size, _value);
+        }
+    }
+}
