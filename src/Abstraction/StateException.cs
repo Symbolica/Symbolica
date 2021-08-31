042e210de5df4f145f0079a92c1bc588db01a73f
Iain Scott <iscott@cantab.net>
2021-08-31 15:44:49 +0100

    Put the stringy bit back into the message.

--- src/Abstraction/StateException.cs
+++ src/Abstraction/StateException.cs
@@ -3,9 +3,11 @@ using Symbolica.Expression;
 
 namespace Symbolica.Abstraction
 {
+    [Serializable]
     public class StateException : Exception
     {
         public StateException(StateError error, ISpace space)
+            : base(error.ToString())
         {
             Error = error;
             Space = space;
