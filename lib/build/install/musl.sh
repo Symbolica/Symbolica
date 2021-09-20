f262ba15e3641c7abf32ed380b42f702b441d998
Matt Thornton <matthew.thornton13@gmail.com>
2021-09-20 19:47:37 +0100

    Clean up musl install scripts in build image.

--- lib/build/install/musl.sh
+++ lib/build/install/musl.sh
@@ -9,5 +9,6 @@ rm -r src/fenv/x86_64 src/ldso src/math/x86_64 src/process/x86_64 src/string/x86
 CODEGEN=1 NO_LLVM=1 CC=~/.symbolica/build/clang CFLAGS="-O0 -Xclang -disable-O0-optnone -fPIC -U__GNUC__" ./configure --disable-shared --prefix=~/.symbolica/build/sysroot
 CODEGEN=1 make -j$(nproc)
 CODEGEN=1 NO_LLVM=1 make install
-find obj/src -name "*.lo.bc" ! -path "obj/src/thread/x86_64/*" ! -name setjmp.lo.bc ! -name longjmp.lo.bc ! -name sigsetjmp.lo.bc ! -name restore.lo.bc -print0 | xargs -0 ~/.symbolica/build/llvm-link -o libc.bc
+mkdir -p ~/.symbolica/build/musl
+find obj/src -name "*.lo.bc" ! -path "obj/src/thread/x86_64/*" ! -name setjmp.lo.bc ! -name longjmp.lo.bc ! -name sigsetjmp.lo.bc ! -name restore.lo.bc -print0 | xargs -0 ~/.symbolica/build/llvm-link -o ~/.symbolica/build/musl/libc.bc
 cd ..
