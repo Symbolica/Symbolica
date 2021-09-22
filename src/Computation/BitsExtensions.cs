ff339953024daba4af08b3c310a8098741080474
Iain Scott <iscott@cantab.net>
2021-09-22 15:09:35 +0100

    Move float parsing nonsense to separate static class. (#72)

--- src/Computation/BitsExtensions.cs
+++ src/Computation/BitsExtensions.cs
@@ -34,83 +34,8 @@ namespace Symbolica.Computation
             {
                 32U => new ConstantSingle(float.Parse(value)),
                 64U => new ConstantDouble(double.Parse(value)),
-                _ => new SymbolicFloat(self, c => self.ParseNormalFloat(c, value))
+                _ => new SymbolicFloat(self, c => FloatParser.ParseNormal(c, self, value))
             };
-        }
-
-        public static FPExpr ParseNormalFloat(this Bits self, Context context, string value)
-        {
-            var sort = self.GetSort(context);
-            var (sign, significand, exponent) = ParseDecimal(value);
-
-            return significand.IsZero
-                ? context.MkFPZero(sort, sign)
-                : CreateFloat(context, sort, sign, significand, exponent);
-        }
-
-        private static FPExpr CreateFloat(Context context, FPSort sort, bool sign, BigInteger significand, int exponent)
-        {
-            var (numerator, denominator) = exponent < 0
-                ? (significand, BigInteger.Pow(10, -exponent))
-                : (significand * BigInteger.Pow(10, exponent), BigInteger.One);
-
-            var precision = (int) sort.SBits;
-            var scale = precision - 1L;
-
-            var lower = BigInteger.One << (precision - 1);
-            while (numerator / denominator < lower)
-            {
-                numerator <<= 1;
-                --scale;
-            }
-
-            var upper = BigInteger.One << precision;
-            while (upper <= numerator / denominator)
-            {
-                denominator <<= 1;
-                ++scale;
-            }
-
-            var quotient = numerator / denominator;
-            var remainder = numerator % denominator;
-            var midpoint = denominator >> 1;
-
-            var (rounded, shift) = remainder > midpoint || remainder == midpoint && !quotient.IsEven
-                ? quotient == upper - BigInteger.One
-                    ? (lower, scale + 1L)
-                    : (quotient + BigInteger.One, scale)
-                : (quotient, scale);
-
-            return context.MkFP(sign, shift, (ulong) (rounded % lower), sort);
-        }
-
-        private static (bool, BigInteger, int) ParseDecimal(string value)
-        {
-            var sign = value[0] == '-';
-
-            var (significand, exponent) = sign
-                ? ParseNonNegativeDecimal(value[1..])
-                : ParseNonNegativeDecimal(value);
-
-            return (sign, significand, exponent);
-        }
-
-        private static (BigInteger, int) ParseNonNegativeDecimal(string value)
-        {
-            var index = value.IndexOfAny(new[] {'e', 'E'});
-
-            return index == -1
-                ? ParseStandardNonNegativeDecimal(value, 0)
-                : ParseStandardNonNegativeDecimal(value[..index], int.Parse(value[(index + 1)..]));
-        }
-
-        private static (BigInteger, int) ParseStandardNonNegativeDecimal(string value, int exponent)
-        {
-            var index = value.IndexOf('.');
-
-            return index == -1
-                ? (BigInteger.Parse(value), exponent)
-                : (BigInteger.Parse(value[..index] + value[(index + 1)..]), exponent - (value.Length - 1 - index));
         }
     }
 }
