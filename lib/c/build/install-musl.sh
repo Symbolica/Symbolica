a8acfbf62488baa537fd5b6d0fcbad9773ecc9ea
Iain Scott <iscott@cantab.net>
2022-01-19 23:30:14 +0000

    Fix path for atomic_arch.

--- lib/c/build/install-musl.sh
+++ lib/c/build/install-musl.sh
@@ -4,7 +4,7 @@ set -e
 git clone --depth 1 --branch v1.2.2 git://git.musl-libc.org/musl
 
 cd musl
-mv ../atomic_arch.h arch/x86_64
+mv ~/.symbolica/atomic_arch.h arch/x86_64
 rm -r src/fenv/x86_64 src/ldso src/math/x86_64 src/process/x86_64 src/string/x86_64
 CODEGEN=1 NO_LLVM=1 CC=~/.symbolica/bin/clang CFLAGS="-O0 -Xclang -disable-O0-optnone -fPIC -U__GNUC__" ./configure --disable-shared --enable-wrapper=clang --prefix=$(eval echo ~/.symbolica)
 CODEGEN=1 make -j$(nproc)
