9c5e4f6c0146d97fd57564aeda6a98e89fcf8908
Matt Thornton <matthew.thornton13@gmail.com>
2022-01-21 13:45:24 +0000

    Add stub definitions for symbolize and make usable from C++. (#114)

--- lib/c/build/include/symbolica.h
+++ lib/c/build/include/symbolica.h
@@ -2,4 +2,15 @@
 
 #define SYMBOLIZE(x) symbolize(&x, sizeof x, #x)
 
-void symbolize(void *address, size_t size, const char *name);
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+    void symbolize(void *address, size_t size, const char *name)
+    {
+    }
+
+#ifdef __cplusplus
+}
+#endif
