5c9a5a56a2dd79d7e4502c2d094bd8e3e7f585fc
Iain Scott <iscott@cantab.net>
2022-01-24 22:20:32 +0000

    Change prefix to write otherwise it makes little sense for mask and value.

--- src/Computation/WriteReader.cs
+++ src/Computation/WriteReader.cs
@@ -5,28 +5,28 @@ namespace Symbolica.Computation
     internal sealed class WriteReader : IReader
     {
         private readonly IReader _defaultReader;
-        private readonly IExpression _previousBuffer;
-        private readonly IExpression _previousMask;
-        private readonly IExpression _previousValue;
+        private readonly IExpression _writeBuffer;
+        private readonly IExpression _writeMask;
+        private readonly IExpression _writeValue;
 
         public WriteReader(IReader defaultReader,
-            IExpression previousBuffer, IExpression previousMask, IExpression previousValue)
+            IExpression writeBuffer, IExpression writeMask, IExpression writeValue)
         {
             _defaultReader = defaultReader;
-            _previousBuffer = previousBuffer;
-            _previousMask = previousMask;
-            _previousValue = previousValue;
+            _writeBuffer = writeBuffer;
+            _writeMask = writeMask;
+            _writeValue = writeValue;
         }
 
         public IExpression Read(ISymbolicExpression buffer, IExpression offset, Bits size)
         {
             var mask = buffer.Mask(offset, size);
 
-            return mask is ConstantExpression && _previousMask is ConstantExpression
-                ? mask.And(_previousMask).Constant.IsZero
-                    ? _previousBuffer.Read(offset, size)
-                    : mask.Xor(_previousMask).Constant.IsZero
-                        ? _previousValue
+            return mask is ConstantExpression && _writeMask is ConstantExpression
+                ? mask.And(_writeMask).Constant.IsZero
+                    ? _writeBuffer.Read(offset, size)
+                    : mask.Xor(_writeMask).Constant.IsZero
+                        ? _writeValue
                         : _defaultReader.Read(buffer, offset, size)
                 : _defaultReader.Read(buffer, offset, size);
         }
