707d766f97961b6e4eec343bee2716b31f452349
Matt Thornton <matthew.thornton13@gmail.com>
2022-02-11 14:24:57 +0000

    Further optimise writes by trying to aggregate overlapping checks for symbolic offsets.

--- src/Computation/Values/Read.cs
+++ src/Computation/Values/Read.cs
@@ -6,12 +6,12 @@ namespace Symbolica.Computation.Values;
 internal static class Read
 {
     public static IValue Create(ICollectionFactory collectionFactory, IAssertions assertions,
-        IValue buffer, IValue offset, Bits size)
+        IValue buffer, IValue offset, Bits size, Write.Overlapping overlapping = Write.Overlapping.Unknown)
     {
         return buffer is IConstantValue b && offset is IConstantValue o
             ? b.AsBitVector(collectionFactory).Read(o.AsUnsigned(), size)
             : buffer is Write w
-                ? w.LayerRead(collectionFactory, assertions, offset, size)
+                ? w.LayerRead(collectionFactory, assertions, offset, size, overlapping)
                 : Truncate.Create(size, LogicalShiftRight.Create(buffer, offset));
     }
 }
--- src/Computation/Values/Write.cs
+++ src/Computation/Values/Write.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Diagnostics;
 using System.Numerics;
 using Microsoft.Z3;
 using Symbolica.Collection;
@@ -9,6 +10,13 @@ namespace Symbolica.Computation.Values;
 
 internal sealed class Write : BitVector
 {
+    public enum Overlapping
+    {
+        Unknown,
+        None,
+        Some
+    }
+
     private readonly IValue _writeBuffer;
     private readonly IValue _writeMask;
     private readonly IValue _writeOffset;
@@ -27,31 +35,55 @@ internal sealed class Write : BitVector
 
     public override string? PrintedValue => null;
 
+    private IValue CumulativeMask => _writeBuffer is Write w ? Or.Create(_writeMask, w.CumulativeMask) : _writeMask;
+
     public override BitVecExpr AsBitVector(IContext context)
     {
         return Flatten().AsBitVector(context);
     }
 
-    public IValue LayerRead(ICollectionFactory collectionFactory, IAssertions assertions, IValue offset, Bits size)
+    public IValue LayerRead(ICollectionFactory collectionFactory, IAssertions assertions,
+        IValue offset, Bits size, Overlapping overlapping)
     {
         var mask = Mask(this, offset, size);
 
+        if (mask is not IConstantValue && (overlapping == Overlapping.None || (overlapping == Overlapping.Unknown && IsNotOverlappingAnyLayer(assertions, mask))))
+        {
+            Debug.Assert(mask is not IConstantValue);
+            return Read.Create(collectionFactory, assertions, _writeBuffer, offset, size, Overlapping.None);
+        }
+
         return IsNotOverlapping(assertions, mask)
-            ? Read.Create(collectionFactory, assertions, _writeBuffer, offset, size)
+            ? Read.Create(collectionFactory, assertions, _writeBuffer, offset, size, Overlapping.Some)
             : IsAligned(assertions, mask)
                 ? _writeValue
-                : Read.Create(collectionFactory, assertions, Flatten(), offset, size);
+                : Read.Create(collectionFactory, assertions, Flatten(), offset, size, Overlapping.Some);
     }
 
-    private IValue LayerWrite(ICollectionFactory collectionFactory, IAssertions assertions, IValue offset, IValue value)
+    private IValue LayerWrite(ICollectionFactory collectionFactory, IAssertions assertions,
+        IValue offset, IValue value, Overlapping overlapping)
     {
         var mask = Mask(this, offset, value.Size);
 
+        if (mask is not IConstantValue && (overlapping == Overlapping.None || (overlapping == Overlapping.Unknown && IsNotOverlappingAnyLayer(assertions, mask))))
+        {
+            Debug.Assert(mask is not IConstantValue);
+            return new Write(Create(collectionFactory, assertions, _writeBuffer, offset, value, Overlapping.None), _writeOffset, _writeValue);
+        }
+
         return IsNotOverlapping(assertions, mask)
-            ? new Write(Create(collectionFactory, assertions, _writeBuffer, offset, value), _writeOffset, _writeValue)
+            ? new Write(Create(collectionFactory, assertions, _writeBuffer, offset, value, Overlapping.Some), _writeOffset, _writeValue)
             : IsAligned(assertions, mask)
                 ? new Write(_writeBuffer, offset, value)
                 : new Write(this, offset, value);
+    }
+
+    private bool IsNotOverlappingAnyLayer(IAssertions assertions, IValue mask)
+    {
+        var isOverlapping = And.Create(mask, CumulativeMask);
+        using var proposition = assertions.GetProposition(isOverlapping);
+
+        return !proposition.CanBeTrue;
     }
 
     private bool IsNotOverlapping(IAssertions assertions, IValue mask)
@@ -83,12 +115,12 @@ internal sealed class Write : BitVector
     }
 
     public static IValue Create(ICollectionFactory collectionFactory, IAssertions assertions,
-        IValue buffer, IValue offset, IValue value)
+        IValue buffer, IValue offset, IValue value, Overlapping overlapping = Overlapping.Unknown)
     {
         return buffer is IConstantValue b && offset is IConstantValue o && value is IConstantValue v
             ? b.AsBitVector(collectionFactory).Write(o.AsUnsigned(), v.AsBitVector(collectionFactory))
             : buffer is Write w
-                ? w.LayerWrite(collectionFactory, assertions, offset, value)
+                ? w.LayerWrite(collectionFactory, assertions, offset, value, overlapping)
                 : new Write(buffer, offset, value);
     }
 }
