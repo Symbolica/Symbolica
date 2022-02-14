32cfd18938674323ef478efdd8c17739c02938b1
Iain Scott <iscott@cantab.net>
2022-02-14 18:39:06 +0000

    Use the space to generalize the layers a bit.

--- src/Computation/Values/Read.cs
+++ src/Computation/Values/Read.cs
@@ -5,12 +5,13 @@ namespace Symbolica.Computation.Values;
 
 internal static class Read
 {
-    public static IValue Create(ICollectionFactory collectionFactory, IValue buffer, IValue offset, Bits size)
+    public static IValue Create(ICollectionFactory collectionFactory, IAssertions assertions,
+        IValue buffer, IValue offset, Bits size)
     {
         return buffer is IConstantValue b && offset is IConstantValue o
             ? b.AsBitVector(collectionFactory).Read(o.AsUnsigned(), size)
             : buffer is Write w
-                ? w.LayerRead(collectionFactory, offset, size)
+                ? w.LayerRead(collectionFactory, assertions, offset, size)
                 : Truncate.Create(size, LogicalShiftRight.Create(buffer, offset));
     }
 }
