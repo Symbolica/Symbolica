49995d6d2cccf850cc9e39d1c0b31bed1a51aed1
Matt Thornton <matthew.thornton13@gmail.com>
2022-03-10 22:44:46 +0000

    Use associativity to merge constants in adjacent Adds. (#172)

--- src/Computation/Values/Add.cs
+++ src/Computation/Values/Add.cs
@@ -19,13 +19,29 @@ internal sealed class Add : BitVector
         return context.CreateExpr(c => c.MkBVAdd(_left.AsBitVector(context), _right.AsBitVector(context)));
     }
 
+    private static IValue ShortCircuit(IValue left, IConstantValue right)
+    {
+        return right.AsUnsigned().IsZero
+            ? left
+            : Create(left, right);
+    }
+
+    private static IValue Create(IValue left, IConstantValue right)
+    {
+        return left switch
+        {
+            IConstantValue l => l.AsUnsigned().Add(right.AsUnsigned()),
+            Add l => Create(l._left, Create(l._right, right)),
+            _ => new Add(left, right)
+        };
+    }
+
     public static IValue Create(IValue left, IValue right)
     {
         return (left, right) switch
         {
-            (IConstantValue l, IConstantValue r) => l.AsUnsigned().Add(r.AsUnsigned()),
-            (IConstantValue l, _) when l.AsUnsigned().IsZero => right,
-            (_, IConstantValue r) when r.AsUnsigned().IsZero => left,
+            (IConstantValue l, _) => ShortCircuit(right, l),
+            (_, IConstantValue r) => ShortCircuit(left, r),
             _ => new Add(left, right)
         };
     }
