de49b45463798697fc09a62a77bf38a4af043292
Matt Thornton <matthew.thornton13@gmail.com>
2022-03-15 20:51:12 +0000

    Try removing apt-get releaseinfo now the build is running again.

--- lib/c/install-focal.sh
+++ lib/c/install-focal.sh
@@ -4,5 +4,5 @@ set -e
 wget -O - https://apt.llvm.org/llvm-snapshot.gpg.key | apt-key add -
 add-apt-repository "deb http://apt.llvm.org/focal/ llvm-toolchain-focal-12 main"
 
-apt-get --allow-releaseinfo-change update
+apt-get update
 apt-get install -y clang-12 make
