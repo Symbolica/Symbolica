de0e22e247a43b42b65511a6fbd831d55e304188
Matt Thornton <matthew.thornton13@gmail.com>
2022-03-17 16:23:32 +0000

    Remove unused import.

--- tests/Application.IntegrationTests/PassingTests.cs
+++ tests/Application.IntegrationTests/PassingTests.cs
@@ -3,7 +3,6 @@ using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using FluentAssertions;
-using Microsoft.VisualStudio.TestPlatform.TestHost;
 using Symbolica.Implementation;
 using Xunit;
 
