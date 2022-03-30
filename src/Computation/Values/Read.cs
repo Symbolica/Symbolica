c17b7aeb187661c7387e6a9f156559865b6b0a28
Matt Thornton <matthew.thornton13@gmail.com>
2022-03-30 12:01:11 +0100

    Rewrite Write to exploit Address values for perf improvement.

--- src/Computation/Values/Read.cs
+++ src/Computation/Values/Read.cs
@@ -1,4 +1,5 @@
 ﻿using Symbolica.Collection;
+using Symbolica.Computation.Exceptions;
 using Symbolica.Computation.Values.Constants;
 using Symbolica.Expression;
 
@@ -9,11 +10,33 @@ internal static class Read
     public static IValue Create(ICollectionFactory collectionFactory, ISolver solver,
         IValue buffer, IValue offset, Bits size)
     {
-        return buffer is IConstantValue b && offset is IConstantValue o
-            ? b.AsBitVector(collectionFactory).Read(o.AsUnsigned(), size)
-            : buffer is Write w
-                ? w.LayerRead(collectionFactory, solver, offset, size)
-                : SymbolicRead(solver, buffer, offset, size);
+        return Create(
+            collectionFactory,
+            solver,
+            buffer,
+            WriteOffsets.Create(offset, buffer.Size, size),
+            size);
+    }
+
+    internal static IValue Create(ICollectionFactory collectionFactory, ISolver solver,
+        IValue buffer, WriteOffsets offsets, Bits size)
+    {
+        if (buffer is Write w)
+            return w.LayerRead(collectionFactory, solver, offsets, size);
+
+        if (offsets.Empty)
+        {
+            if (buffer.Size != size)
+                throw new InconsistentExpressionSizesException(buffer.Size, size);
+
+            return buffer;
+        }
+
+        var offset = offsets.Head();
+        var subBuffer = buffer is IConstantValue b && offset.Value is IConstantValue o
+            ? b.AsBitVector(collectionFactory).Read(o.AsUnsigned(), offset.FieldSize)
+            : SymbolicRead(solver, buffer, offset.Value, offset.FieldSize);
+        return Create(collectionFactory, solver, subBuffer, offsets.Tail(), size);
     }
 
     private static IValue SymbolicRead(ISolver solver, IValue buffer, IValue offset, Bits size)
@@ -23,7 +46,7 @@ internal static class Read
             LogicalShiftRight.Create(buffer, Resize.Create(buffer.Size, offset)));
 
         return solver.TryGetSingleValue(value, out var constant)
-            ? ConstantUnsigned.Create(value.Size, constant)
+            ? ConstantUnsigned.Create(size, constant)
             : value;
     }
 }
